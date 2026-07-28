bcec74d9191cdc194059198435c874891b058cfa
swtpoonamsingh78626-git <swt.poonamsingh78626@gmail.com>
2026-07-28 21:46:30 +0530

    Delete loops.java

--- loops.java
+++
@@ -1,7 +0,0 @@
-public class loops{
-    public static void main(String args[]){
-        for(int counter=0;counter<100;counter=counter+1){
-            System.out.println("hello world");
-        }
-    }
-}
